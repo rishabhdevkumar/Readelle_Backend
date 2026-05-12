ca99b70530370a1e9668ffbca7c61928a5dc6c12
Saroj_Dev <sarojkumardas.dev@gmail.com>
2026-05-12 13:45:54 +0530

    Keep the routes in order

--- src/index.js
+++ src/index.js
@@ -2,18 +2,16 @@ const express = require('express');
 const serverConfig = require('./config/server.config');
 const connectDB = require('./config/db.config');
 const bookRoutes = require("./routes/book.routes");
+const userRouter = require("./routes/user.routes");
 
 const app = express();
 app.use(express.json());
 
-app.use("/api/books", bookRoutes);
 
 app.use(express.json());
 
-
-
 app.use("/api/users",userRouter);
-
+app.use("/api/books", bookRoutes);
 
 
 app.get('/ping', (req, res) => {
