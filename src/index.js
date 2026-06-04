1d5f28c65d2c32d88d6744efca57b5993dd8347e
Rishabhdevkumar <rishabhdevkumar77@gmail.com>
2026-06-04 13:17:07 +0530

    fix login error

--- src/index.js
+++ src/index.js
@@ -17,7 +17,8 @@ const bookmarkRouter = require("./routes/bookmark.routes");
 const app = express();
 
 app.use(cors({
-    origin: '*',
+    origin: "http://localhost:5173",
+    credentials: true,
     methods: 'GET,POST,PUT,PATCH,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
 }));
